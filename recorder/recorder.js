008f0eff43b4a849f6315be293faae2b6c15fc2a
jakubfiala <my email>
2018-06-23 15:00:45 +0100

    added recorder app

---
+++ recorder/recorder.js
@@ -0,0 +1,69 @@
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+const recordButton = document.getElementById('record-button');
+const UPLOAD_BASE_PATH = '/upload-recording';
+
+const getUploadURL = ({ timestamp, lat, lng }) => {
+  return `${UPLOAD_BASE_PATH}?timestamp=${timestamp}&lat=${lat}&lng=${lng}`;
+};
+
+const saveBlobAtPosition = blob => position => {
+  console.log(blob, position);
+
+  const metadata = {
+    timestamp: position.timestamp,
+    lat: position.coords.latitude,
+    lng: position.coords.longitude
+  };
+
+  fetch(getUploadURL(metadata), { method: 'PUT', body: blob, mode: 'same-origin' })
+    .then(response => response.ok && console.log('upload successful'))
+    .catch(err => console.error(`Upload error: ${err}`));
+};
+
+const saveRecording = (recorder, blob) => {
+  navigator.geolocation
+    .getCurrentPosition(
+      saveBlobAtPosition(blob),
+      err => console.error(err),
+      { enableHighAccuracy: true });
+};
+
+const stopRecording = recorder => {
+  return function stopHandler(e) {
+    recorder.onComplete = saveRecording;
+    recorder.finishRecording();
+
+    const button = e.target;
+    button.innerText = 'Start Recording';
+    button.removeEventListener('click', stopHandler);
+    button.addEventListener('click', startRecording(recorder));
+  };
+};
+
+const startRecording = recorder => {
+  return function startHandler(e) {
+    const button = e.target;
+    recorder.startRecording();
+    button.innerText = 'Stop Recording';
+
+    button.removeEventListener('click', startHandler);
+    button.addEventListener('click', stopRecording(recorder));
+  };
+};
+
+const initialiseRecorder = stream => {
+  recordButton.hidden = false;
+
+  const audio = new AudioContext();
+  const source = audio.createMediaStreamSource(stream);
+
+  const recorder = new WebAudioRecorder(source, { workerDir: "lib/web-audio-recorder/" });
+  recorder.setEncoding('mp3');
+
+  recordButton.addEventListener('click', startRecording(recorder));
+};
+
+navigator.mediaDevices
+  .getUserMedia({ audio: true, video: false })
+  .then(initialiseRecorder);
