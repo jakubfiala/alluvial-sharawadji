09737401ff4a3b1219df7ab69f943e6fd0c6b1eb
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2018-08-11 11:14:55 +0100

    fix timestamp

--- middleware/save-recording.js
+++ middleware/save-recording.js
@@ -36,7 +36,7 @@ const saveRecording = async (req, res) => {
             soundwalk_name: soundwalk,
             lat: req.query.lat,
             lng: req.query.lng,
-            timestamp: new Date(req.query.timestamp).toISOString(),
+            timestamp: new Date(req.query.timestamp.parseInt()).toISOString(),
             src: SOUND_URL_PREFIX.concat(Key),
             db: 80,
             loop: true
