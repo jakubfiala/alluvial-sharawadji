88637ebb4389390f50923db984f299aa7cf6b6fd
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2023-11-17 18:16:58 +0000

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source .env
 npm install
-pm2 delete 'alluvial-sharawadji'
-knex migrate:latest
-knex seed:run
+pm2 delete 'alluvial-sharawadji' || true
+#knex migrate:latest
+#knex seed:run
 pm2 start npm --name 'alluvial-sharawadji' -- start
